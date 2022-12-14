d477b601f0be9bbcf2abea138bb056dc9b00b9eb
Guillaume Thomas <guillaume.thomas@axa.fr>
2022-12-14 10:15:55 +0100

    fix: scope in claims of the token can be an array

--- src/tests/test_authentication.py
+++ src/tests/test_authentication.py
@@ -3,7 +3,9 @@ import unittest
 from src.oidc_jwt_validation import authentication
 
 VALID_SCOPE = "test_scope"
-INVALID_SCOPE = "test_invalid_scope"
+ARRAY_OF_VALID_SCOPES = "scope1 test_scope scope2"
+INVALID_SCOPE = "test_scope_invalid"
+ARRAY_OF_INVALID_SCOPES = "scope1 test_scope_invalid scope3"
 
 
 class AuthenticationTestCase(unittest.TestCase):
@@ -13,9 +15,21 @@ class AuthenticationTestCase(unittest.TestCase):
         }
         self.assertEqual(authentication.is_scope_valid(payload, VALID_SCOPE), True)
 
+    def test_scope_validation_with_valid_scope_in_array_of_scopes_returns_true(self):
+        payload = {
+            "scope": ARRAY_OF_VALID_SCOPES
+        }
+        self.assertEqual(authentication.is_scope_valid(payload, VALID_SCOPE), True)
+
     def test_scope_validation_with_invalid_scope_returns_false(self):
         payload = {
             "scope": INVALID_SCOPE
+        }
+        self.assertEqual(authentication.is_scope_valid(payload, VALID_SCOPE), False)
+
+    def test_scope_validation_with_invalid_scope_in_array_of_invalid_scopes_returns_false(self):
+        payload = {
+            "scope": ARRAY_OF_INVALID_SCOPES
         }
         self.assertEqual(authentication.is_scope_valid(payload, VALID_SCOPE), False)
 
